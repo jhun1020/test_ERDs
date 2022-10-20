e82023d2de98f2fc5e6686a7699e0c166bc894e1
jhun1020 <jhun1020.kh@gmail.com>
2022-10-20 12:57:51 +0900

    jhun

--- prac_erd/Products_w3school/Products_CategoryID.sql
+++
@@ -1,7 +0,0 @@
--- Products에 CATEGORY_ID 가 5,6를 표시, 내용 : 프로덕트 이름 공급자 이름 출력
-SELECT Products.ProductName, Suppliers.SupplierName
-FROM Products INNER JOIN Suppliers
-ON Products.SupplierID = Suppliers.SupplierID
-WHERE CategoryID IN(5,6)
-;
-
