ac7cc19761975b735fbe42f181f235031d15ed54
jhun1020 <jhun1020.kh@gmail.com>
2022-10-20 13:34:02 +0900

    jhun1020_1020

--- prac_erd/w3school/Products_CatgoryID.sql
+++ prac_erd/w3school/Products_CatgoryID.sql
@@ -1,4 +1,5 @@
 -- Products에 CATEGORY_ID 가 5,6를 표시, 내용 : 프로덕트 이름 공급자 이름 출력
+-- 결과 : 13
 SELECT Products.ProductName, Suppliers.SupplierName
 FROM Products INNER JOIN Suppliers
 ON Products.SupplierID = Suppliers.SupplierID
